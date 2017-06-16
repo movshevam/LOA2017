e44996c77ea7815c5ae6d8065d520d2395864d98
Maria Movsheva <movshevam@gmail.com>
2017-06-16 20:42:17 +0200

    added comments and changed name

---
+++ javacopy/ch3/typeMethodsParamInt.java
@@ -0,0 +1,17 @@
+/*Methods type void and int, using int*/
+class typeMethodsParamInt{
+  public static void main(String[] args){
+    int a = 6;
+    foo(a);
+    System.out.println(a);
+    a = goo(a);
+    System.out.println(a);
+  }
+  public static void foo(int a){
+    a++;
+  }
+  public static int goo(int a){
+    a = 5;
+    return a;
+  }
+}
