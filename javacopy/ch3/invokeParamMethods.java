cf1872d214f88c7a6afa35e83624454e1f9e220b
Maria Movsheva <movshevam@gmail.com>
2017-06-16 20:24:10 +0200

    added a comment

--- javacopy/ch3/invokeParamMethods.java
+++ javacopy/ch3/invokeParamMethods.java
@@ -3,6 +3,7 @@ class invokeParamMethods{
   public static void main(String[] args){
     zool(11, "cactus", "Sun street");
   }
+  //this is a method
   public static void zool(int a, String petname, String street){
     System.out.println(a + " " + petname + " " + street);
   }
