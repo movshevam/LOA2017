20825b8bb8e3e72c51df14761f5c019da097a228
Maria Movsheva <movshevam@gmail.com>
2017-06-16 20:15:50 +0200

    commit deletion of files

--- javacopy/ch3/zool.java
+++
@@ -1,8 +0,0 @@
-class zool{
-  public static void main(String[] args){
-    zool(11, "cactus", "Sun street");
-  }
-  public static void zool(int a, String petname, String street){
-    System.out.println(a + " " + petname + " " + street);
-  }
-}
