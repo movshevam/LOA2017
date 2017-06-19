a7faa90bd10561c06167b632aaa9894caa87c4ad
Maria Movsheva <movshevam@gmail.com>
2017-06-19 12:24:34 +0200

    variable names

---
+++ javacopy/exam1/LegalVariableNames.java
@@ -0,0 +1,8 @@
+class LegalVariableNames{
+  public static void main(String[] args) {
+    int _INT_ = 5; //Valid
+    int $bash = 4; //Valid, but never used by convention
+    int publis = 2; //more common
+    System.out;
+  }
+}
