0c42c6e61e22e4ad0b2036c46f19a63a62d6e30b
Maria Movsheva <movshevam@gmail.com>
2017-06-19 14:52:18 +0200

    excercise 11

---
+++ javacopy/ch12/ex12_11LetterHist.java
@@ -0,0 +1,16 @@
+class ex12_11LetterHist{
+  public static void main(String[] args) {
+    String input = "abracadabra";
+  }
+  public static int[] letterHist(String str){
+    int[] histogram = new int[str.length()];
+    int count =0;
+    for(int i=0; i<str.length;i++){
+      
+    }
+    return histogram;
+  }
+  public static int findLetter(String str, char a){
+    return -1;
+  }
+}
